575aabc8178d2b76940ef8190b7dd9b882e3e9bf
Enzo Antônio Ferreira de Araújo - SENAI <enzo.a.araujo6@aluno.senai.br>
2025-04-07 15:35:18 -0300

    criação do código.py
    Criação de um código py

---
+++ Códigos Python/código-brabo.py
@@ -0,0 +1,6 @@
+#-*-coding:UTF-8-*-
+print("Bem-vindo(a) ao centro de treinamento para o seu personagem de RPG! Aqui você poderá exercitar seu combatente para aumentar seus atributos!")
+nome=str(input("Escreva o nome do seu personagem: "))
+atr=str(input("Escreva o atributo que você quer aumentar. Digite 'V' para velocidade, 'F' para força e 'I' para inteligência: "))
+pon=int(input("Agora insira quantos pontos de atributo você quer utilizar: "))
+
